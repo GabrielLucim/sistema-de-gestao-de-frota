a05a5646cae4d6bd171afe8ea39e373bf6a699ba
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2025-09-24 22:31:01 -0300

    Add files via upload

---
+++ GestaoFrota/src/main/java/model/Veiculo.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Veiculo {
+	private Long id;
+	private String placa;
+	private String modelo;
+	private String fabricante;
+	private int anoFabricacao;
+	private double kmAtual;
+	private double consumoMedio;
+	private StatusVeiculo status;
+}
